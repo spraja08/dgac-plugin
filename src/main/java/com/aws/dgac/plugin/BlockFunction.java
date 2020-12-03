fc95ed3a7fae28e675c7c1cf8599df11dd01f329
Sp <rspamzn@a483e736f3f7.ant.amazon.com>
2020-12-04 06:29:57 +0800

    data types support added

--- src/main/java/com/aws/dgac/plugin/BlockFunction.java
+++ src/main/java/com/aws/dgac/plugin/BlockFunction.java
@@ -5,17 +5,38 @@ package com.aws.dgac.plugin;
 
 import com.facebook.presto.spi.function.Description;
 import com.facebook.presto.spi.function.ScalarFunction;
+import com.facebook.presto.spi.function.SqlNullable;
 import com.facebook.presto.spi.function.SqlType;
+import com.facebook.presto.spi.function.TypeParameter;
 import com.facebook.presto.spi.type.StandardTypes;
 import io.airlift.slice.Slice;
 import io.airlift.slice.Slices;
 
 //Build using clean package -Dcheckstyle.skip=true -Dcheckstyle.skipExec=true
+@Description("Block Function")
+@ScalarFunction(alias="aws_dgac_block", calledOnNullInput=true) // method name
 public class BlockFunction {
-    @Description("Block Function")
-    @ScalarFunction("aws_dgac_block") // method name
-    @SqlType(StandardTypes.VARCHAR) // Return type
-    public static Slice aws_dgac_block(@SqlType(StandardTypes.VARCHAR) Slice str) {
+    @TypeParameter("T")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice aws_dgac_block(@SqlNullable @SqlType("T") Slice str) {
+        return Slices.utf8Slice("blocked");
+    }
+
+    @TypeParameter("T")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice aws_dgac_block(@SqlNullable @SqlType("T") Long str) {
+        return Slices.utf8Slice("blocked");
+    }
+
+    @TypeParameter("T")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice aws_dgac_block(@SqlNullable @SqlType("T") Double str) {
+        return Slices.utf8Slice("blocked");
+    }
+
+    @TypeParameter("T")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice aws_dgac_block(@SqlNullable @SqlType("T") Integer str) {
         return Slices.utf8Slice("blocked");
     }
 }
